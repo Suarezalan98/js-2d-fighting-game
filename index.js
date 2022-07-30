1dcd3e40a610553849ccc67f5789c61a16767629
Alan Suarez <alansuarez@Alans-MacBook-Pro.local>
2022-07-30 10:55:18 -0700

    added health bar, game timer, game over

--- index.js
+++ index.js
@@ -139,6 +139,35 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
   );
 }
 
+function determineWinner({ playerOne, playerTwo, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector("#displayText").style.display = "flex";
+  if (playerOne.health === playerTwo.health) {
+    document.querySelector("#displayText").innerHTML = "Tie";
+  } else if (playerOne.health > playerTwo.health) {
+    document.querySelector("#displayText").innerHTML = "Player 1 Wins";
+  } else if (playerTwo.health > playerOne.health) {
+    document.querySelector("#displayText").innerHTML = "Player 2 Wins";
+  }
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+
+  if (timer === 0) {
+    document.querySelector("#displayText").style.display = "flex";
+    determineWinner({ playerOne, playerOne, timerId });
+  }
+}
+
+decreaseTimer();
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
@@ -187,6 +216,11 @@ function animate() {
     playerOne.health -= 20;
     document.querySelector("#playerOneHealth").style.width =
       playerOne.health + "%";
+  }
+
+  // end game based on health
+  if (playerTwo.health <= 0 || playerOne.health <= 0) {
+    determineWinner({ playerOne, playerTwo, timerId });
   }
 }
 
