8e5049f102671f965cbda7d4300a9d5a6bd90a49
Alan Suarez <alansuarez@Alans-MacBook-Pro.local>
2022-09-29 15:31:31 -0700

    increased opacity for overlay

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   shop.update();
-  c.fillStyle = "rgba(255, 255, 255, 0.08)";
+  c.fillStyle = "rgba(255, 255, 255, 0.15)";
   c.fillRect(0, 0, canvas.width, canvas.height);
   playerOne.update();
   playerTwo.update();
